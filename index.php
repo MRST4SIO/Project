a6e3a9eb3369c1e594e6ef3546e6d8b287e0aacd
MRST4SIO <mrstasio1@gmail.com>
2024-09-17 22:06:14 +0200

    o

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Document</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel="stylesheet" href="css/main.css">
-</head>
+<html lang="pl">
+
 
 <body>
 
+<form action="welcome.php" method="POST">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
     
+
 
 
 </body>
---
+++ welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+
+<body>
+
+<h1>Twoje imie to: $name</h1>
+
+<?php 
+    $name = $_POST['fname'];
+?>
+
+
+
+</body>
+
+</html>
